68a4f135ff5190bc27a700b32c0eb0ff6031383d
Angelica Velez <avelez9882@gmail.com>
2020-09-02 11:17:55 -0400

    Initial commit

---
+++ AV_FinancialPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AV_FinancialPortal.Startup))]
+namespace AV_FinancialPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
